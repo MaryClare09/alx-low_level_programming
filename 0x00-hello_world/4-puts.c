4b8abf038069d585cd9ebc3ccb59aa52b81f4058
Amaka <claremary360@gmail.com>
2022-09-08 05:16:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main - entry point
  *
- *  * main - main function
+ * Return: 0 after printing the function
  *
- *   * Return: Always 0.
- *
- *   */
-
+ */
 int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-} 
+{puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}           
