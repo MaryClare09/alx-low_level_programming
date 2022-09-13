0acddaa628130a5b7785ee62834db43b96c957ed
Amaka <claremary360@gmail.com>
2022-09-13 13:54:10 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,14 +7,14 @@
  * Return: 1 if lowercase
  */
 
-int _islower(int c) 
+int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z') 
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
-	} 
+	}
 	else
 	{
 		return (0);
-	} 
-} 
+	}
+}
